cf48f5e94cdb62299c0ece1fd1e6d59c61a5387e
Juhász Zoltán Admin <juhaszz.admin@vereb.dc>
2020-09-28 08:43:58 +0200

    Add project files.

---
+++ CA200928/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200928
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var s = "Hello World!";
+
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth - s.Length),
+                    rnd.Next(Console.WindowHeight));
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.Write(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
